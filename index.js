675e1b7b7d8e7614c39c36b9aef222e47c406718
Yancy <yancyymata0@gmail.com>
2026-03-04 15:03:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let username;
+
+document.getElementById("Submit").onclick = function() {
+    username = document.getElementById("Text").value;
+    document.getElementById("display").textContent;
+}
