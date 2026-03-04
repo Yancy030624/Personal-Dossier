ab88a35ea76e5f9469f67c52e8210b71946eec29
Yancy <yancyymata0@gmail.com>
2026-03-04 15:04:20 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let username;
-
-document.getElementById("Submit").onclick = function() {
-    username = document.getElementById("Text").value;
-    document.getElementById("display").textContent;
-}
